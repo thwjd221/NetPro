f083b20c15f849361a3947c321102a96f3c8e5a7
jssister0803 <jssister0803@gmail.com>
2021-05-21 16:21:42 +0900

    test

--- ReceiveThread.java
+++ ReceiveThread.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
-	//test
+	
 public class ReceiveThread extends Thread{
 	Socket socket = null;
 	

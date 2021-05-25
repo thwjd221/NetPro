4ec674ed768e69c8b24d52acb5fd76cbb2eb351c
thwjd221 <thwjd5249@gmail.com>
2021-05-26 03:06:52 +0900

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,33 @@
-/*test*/
-public class Server {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class Server {
+	static ArrayList total_socket = new ArrayList();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//test
+		ServerSocket ss;
+		try {
+			ss = new ServerSocket(8888);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (true) {
+			Socket server;
+			try {
+				server = ss.accept();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+			total_socket.add(server);
+			//new ServerThread(server).start();
+		}
+
 	}
 
 }

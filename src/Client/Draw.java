977b57b82f89f05c0d9a0dbc1a8bff4c4e979781
6molleh <60yuiop@gmail.com>
2021-05-27 01:56:44 +0900

    Create Client(Login, Channel, Room)
    디자인 임시로 넣음

---
+++ src/Client/Draw.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class Draw extends JPanel{
+    private Vector<Point> xy = new Vector<Point>();
+        
+    public Draw() {
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) { 
+                xy.add(e.getPoint());
+                repaint();
+            }
+        });
+            
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                xy.add(null);
+                xy.add(e.getPoint());
+            }
+        });
+    }
+        
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int i = 1; i < xy.size(); i++) {
+           if (xy.get(i - 1) == null)
+                continue;
+            else if (xy.get(i) == null)
+                continue;
+            else
+                g.drawLine((int) xy.get(i - 1).getX(), (int) xy.get(i - 1).getY(),
+                (int) xy.get(i).getX(), (int) xy.get(i).getY());
+        }
+    }
+}

55301d91ad0dfd4c9564237f2670c1c402504520
thwjd221 <thwjd5249@gmail.com>
2021-05-26 12:01:27 +0900

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,41 @@
+import java.awt.Image;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+class ServerThread extends Thread{
+	Socket server;
+	String[] subject = new String[] {"cat","school","sandwich","friend"};
+	int i = 0;
+	//string -> byte 변환
+	byte[] transstr = subject[i].getBytes();
+	
+	public ServerThread (Socket server) {
+		this.server = server;
+	}
+	
+	public void run() {
+		//주제 전달
+		Socket givesub = (Socket) Server.total_socket.get(i);
+		try {
+			givesub.getOutputStream().write(transstr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			ObjectInputStream ois = new ObjectInputStream(givesub.getInputStream());
+			//Image img =  (Image)ois.readObject();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
 
 public class Server {
 	static ArrayList total_socket = new ArrayList();
